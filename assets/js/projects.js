f9cd31e1b65eba9f56a612e5818c434a57cea993
MSFPT <MsfNemati1386@gmail.com>
2022-06-15 15:39:23 -0300

    Updated PWA&Styles&Js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,4 +1,5 @@
 var e_request = new XMLHttpRequest();
+const project_box = document.querySelector("#project-list");
 
 e_request.onreadystatechange = function () {
 
@@ -7,8 +8,6 @@ e_request.onreadystatechange = function () {
         let _data = JSON.parse(this.responseText);
 
         document.querySelector("#projects .title a sup").innerText = _data.length
-
-        const project_box = document.querySelector("#project-list");
 
         for (let i = 0; i < _data.length; i++) {
             const _i = _data[i];
@@ -27,6 +26,42 @@ e_request.onreadystatechange = function () {
     }
 };
 
-let repos_github_api = "https://api.github.com/users/msfpt/repos?sort=last_updates";
-e_request.open("GET", repos_github_api, true);
-e_request.send();
+const start_page = () => {
+
+    let repos_github_api = "https://api.github.com/users/msfpt/repos?sort=last_updates";
+
+    if (navigator.onLine ? true : false) {
+        e_request.open("GET", repos_github_api, true);
+        e_request.send();
+    } else {
+        document.querySelector('#projects').style.marginTop = '10em'
+
+        const title_pb = document.querySelector('#projects .title a')
+        title_pb.removeAttribute('href');
+        title_pb.innerText = 'No internet'
+        title_pb.style.color = 'red'
+
+        const network_error_text = document.createElement('h2');
+        network_error_text.style.margin = '2em'
+        network_error_text.style.textAlign = 'center'
+        network_error_text.innerText = 'Checking the network cables, modem, and router \n Reconnecting to Wi-Fi'
+        console.error('ERR_INTERNET_DISCONNECTED');
+
+        const reload_button = document.createElement('button');
+        reload_button.classList.add('btn')
+        reload_button.innerText = 'Reload'
+        reload_button.style.color = 'black'
+        reload_button.addEventListener('click', (event) => {
+            location.reload();
+        });
+
+        project_box.appendChild(network_error_text);
+        document.querySelector('#projects .btn-box').appendChild(reload_button);
+    }
+}
+
+start_page();
+setInterval(() => {
+    project_box.innerHTML = ''
+    start_page();
+}, 300000); // Every five minutes 
--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,7 +1,135 @@
+
+const staticCacheName = "site-static-v1";
+
+const cachePages = [
+    "/",
+    "/index.html",
+    "/tutorials/index.html",
+    "/projects/index.html",
+    "/about/index.html",
+    "/404.html",
+]
+
+const cacheFiles = [
+    "manifest.json",
+    "/404.md",
+    "/README.md",
+    "/LICENSE"
+]
+
+const cacheAssetsJs = [
+    // "/assets/js/service-worker.js",
+    "/assets/js/script.js",
+    "/assets/js/menu.js",
+    "/assets/js/tutorials.js",
+    "/assets/js/projects.js",
+]
+
+const cacheAssetsCss = [
+    "/assets/css/base/main.css",
+    "/assets/css/base/font.css",
+    "/assets/css/base/header.css",
+    "/assets/css/base/menu.css",
+    "/assets/css/base/theme.css",
+    "/assets/css/base/aos-2.3.1.css",
+
+    "/assets/css/page/index.css",
+    "/assets/css/page/error.css",
+    "/assets/css/page/tutorials.css",
+    "/assets/css/page/projects.css",
+    "/assets/css/page/about.css",
+]
+
+const cacheAssetsFont = [
+    "/assets/font/HelpMe-owODB.ttf",
+    "/assets/font/iransans.ttf",
+    "/assets/font/IRANSansWeb.woff2",
+    "/assets/font/iranyekanwebboldfanum.woff",
+    "/assets/font/iranyekanwebregularfanum.woff",
+    "/assets/font/Library_3_am.otf",
+    "/assets/font/NightmarePills-BV2w.ttf",
+    "/assets/font/p9e.ttf",
+    "/assets/font/Potra.ttf",
+    "/assets/font/Roboto-Thin.ttf",
+    "/assets/font/Vazir-FD.woff",
+    
+    "/assets/font/poppins/Poppins-Bold.ttf",
+    "/assets/font/poppins/Poppins-Light.ttf",
+    "/assets/font/poppins/Poppins-Medium.ttf",
+    "/assets/font/poppins/Poppins-Regular.ttf",
+    "/assets/font/poppins/Poppins-SemiBold.ttf",
+
+    "/assets/font/roboto/Roboto-Black.ttf",
+    "/assets/font/roboto/Roboto-BlackItalic.ttf",
+    "/assets/font/roboto/Roboto-Bold.ttf",
+    "/assets/font/roboto/Roboto-BoldCondensed.ttf",
+    "/assets/font/roboto/Roboto-BoldCondensedItalic.ttf",
+    "/assets/font/roboto/Roboto-BoldItalic.ttf",
+    "/assets/font/roboto/Roboto-Condensed.ttf",
+    "/assets/font/roboto/Roboto-BoldItalic.ttf",
+    "/assets/font/roboto/Roboto-CondensedItalic.ttf",
+    "/assets/font/roboto/Roboto-Italic.ttf",
+    "/assets/font/roboto/Roboto-Light.ttf",
+    "/assets/font/roboto/Roboto-LightItalic.ttf",
+    "/assets/font/roboto/Roboto-Medium.ttf",
+    "/assets/font/roboto/Roboto-MediumItalic.ttf",
+    "/assets/font/roboto/Roboto-Regular.ttf",
+    "/assets/font/roboto/Roboto-Thin.ttf",
+    "/assets/font/roboto/Roboto-ThinItalic.ttf",
+]
+
+const cacheAssetsImages = [
+    "/favicon.ico",
+    "/assets/icons/app-icon-16x16.png",
+    "/assets/icons/app-icon-32x32.png",
+    "/assets/icons/app-icon-64x64.png",
+    "/assets/icons/app-icon-128x128.png",
+    "/assets/icons/app-icon-192x192.png",
+    "/assets/icons/app-icon-256x256.png",
+    "/assets/icons/app-icon-512x512.png",
+    "/assets/icons/apple-touch-icon.png",
+
+    "/assets/img/banner.png",
+    "/tutorials/assets/img/tutorials.png",
+    "/assets/img/books.jpg",
+    "/assets/img/projects.jpg",
+    // "/assets/img/articles.jpg",
+]
+
+const cacheAssets = cacheAssetsImages + cacheAssetsJs + cacheAssetsFont + cacheAssetsCss ;
+    
+const cacheData = cachePages + cacheFiles + cacheAssets ;
+
 self.addEventListener("install", event => {
-    console.log("Service Worker installing.");
+    console.log("Service Worker installing...");
+    event.waitUntil(
+        caches
+            .open(staticCacheName)
+            .then(cache => {
+                console.log("caching data...");
+                cache.addAll(cacheData);
+            })
+            .catch(error => {
+                console.error("Service Worker error");
+            })
+    );
 });
 
 self.addEventListener("activate", event => {
-    console.log("Service Worker activating.");
+    console.log("Service Worker activated.");
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(res => {
+                return res || fetch(event.request);
+            })
+        .catch(error => {
+            if (event.request.url.indexOf(".html") > -1) {
+                return caches.match("/index.html");
+            }
+        })
+    );
 });
